aa118bc1109fc48ac48cd4678d990beaea36dcfc
BarbonFisso <barbieri.mat@hotmail.it>
2021-12-01 14:50:12 +0100

    added api call for movies list

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,17 +1,45 @@
 <template>
     <header>
-        <input type="text" v-model="textSerch">
-        <button @click.prevent="">Cerca</button>
+        <input 
+            type="text" 
+            v-model="textSearch"
+            @keyup.enter="getMovies"
+        >
+        <button @click.prevent="getMovies">
+            Cerca
+        </button>
     </header>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
     name : 'App',
 
     data () {
         return {
-            textSerch : "",
+            textSearch : "",
+            // dati api
+            apiUrl: "https://api.themoviedb.org/3/",
+            apiKey: 'api_key=327423a3fa358699db3662c3a02c5b5e',
+            apiMoviesSerch: 'search/movie?',
+
+            movies: []
+
+        }
+    },
+    methods: {
+        getMovies() {
+            axios
+            .get(`${this.apiUrl}${this.apiMoviesSerch}${this.apiKey}&query=${this.textSearch}`)
+            .then((result) => {
+                this.movies = result.data.results;
+                console.log(this.movies);
+            })
+            .catch((err) => {
+                console.log(err);
+            })
         }
     }
 }
