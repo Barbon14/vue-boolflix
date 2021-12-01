17b2f9ad1b7433e1e6d3515d9afcd6789b7ea7ea
BarbonFisso <barbieri.mat@hotmail.it>
2021-12-01 14:21:08 +0100

    added AppHeader

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,22 @@
+<template>
+    <header>
+        <input type="text" v-model="textSerch">
+        <button @click.prevent="">Cerca</button>
+    </header>
+</template>
+
+<script>
+export default {
+    name : 'App',
+
+    data () {
+        return {
+            textSerch : "",
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
