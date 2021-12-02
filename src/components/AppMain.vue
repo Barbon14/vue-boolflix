003a47a888ffb58fa02fc1091b98a7e9ade6f13f
BarbonFisso <barbieri.mat@hotmail.it>
2021-12-02 16:58:44 +0100

    Fix: layout AppMain

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -4,21 +4,25 @@
             <h2>
                 Film
             </h2>
-            <Card 
-                v-for="movie in moviesList" 
-                :key="movie.id"
-                :info="movie"
-            />
+            <div class="container">
+                <Card 
+                    v-for="movie in moviesList" 
+                    :key="movie.id"
+                    :info="movie"
+                />
+            </div>
         </section>
         <section class="tv-series">
             <h2>
                 Serie TV
             </h2>
-            <Card 
-                v-for="show in tvSeriesList" 
-                :key="show.id"
-                :info="show"
-            />
+            <div class="container">
+                <Card 
+                    v-for="show in tvSeriesList" 
+                    :key="show.id"
+                    :info="show"
+                />
+            </div>
         </section>
     </main>
 </template>
@@ -39,5 +43,16 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+main {
+    height: calc(100vh - 80px);
+    background-color: #1b1b1b;
+    overflow-y: auto;
+    color: white;
 
+    .container {
+        padding: 20px 0;
+        display: flex;
+        overflow-x: auto;
+    }
+}
 </style>
